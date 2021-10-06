16991844c2d249299c8c7717a2649d5494215060
Alexey Verein <tpom6oh@gmail.com>
2021-10-06 22:01:21 +0300

    Some type converter tests

--- test/avo_inspector_test.dart
+++ test/avo_inspector_test.dart
@@ -9,4 +9,52 @@ void main() {
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
   });
+
+  test('int parameters are extracted as "int"', () {
+    final result = extractType(1);
+
+    expect(result, "int");
+  });
+
+  test('double parameters are extracted as "float"', () {
+    final result = extractType(1.0);
+
+    expect(result, "float");
+  });
+
+  test('string parameters are extracted as "string"', () {
+    final result = extractType("hello world");
+
+    expect(result, "string");
+  });
+
+  test('boolean parameters are extracted as "bool"', () {
+    final result = extractType(true);
+
+    expect(result, "bool");
+  });
+
+  test('list parameters are extracted as "list"', () {
+    final result = extractType([1,2,3]);
+
+    expect(result, "list");
+  });
+
+  test('set parameters are extracted as "list"', () {
+    final result = extractType({"one", "two", "three"});
+
+    expect(result, "list");
+  });
+
+  test('map parameters are extracted as "object"', () {
+    final result = extractType({"one": 1, "two": 2, "three": 3});
+
+    expect(result, "object");
+  });
+
+  test('Random object parameters are extracted as "unknown"', () {
+    final result = extractType(Calculator());
+
+    expect(result, "unknown");
+  });
 }
